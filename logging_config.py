c87a4ae176958a1bcf2f233dba4a91a2ce1c07f0
Yaroslav Benin <dondublon1@yandex.ru>
2026-05-09 18:24:55 +0300

    + logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,7 @@
+from loguru import logger
+from dotenv import load_dotenv
+from sys import stderr
+from os import getenv
+load_dotenv()
+logger.remove()
+logger.add(stderr, level=getenv("LOG_LEVEL", "INFO"))
